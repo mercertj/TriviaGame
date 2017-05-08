e9828643041f0e897557961d2f5da11c387e274c
Thomas Mercer <tmercer74@gmail.com>
2017-05-08 10:31:32 -0700

    new folders

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,39 @@
+
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+document.getElementById('timer').innerHTML =
+  03 + ":" + 00;
+startTimer();
+
+function countDown(mins, secs) {
+  var presentTime = document.getElementById("timer").innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){alert("Time's Up!")}
+  
+  document.getElementById("timer").innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
